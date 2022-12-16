4d36352799bb5fe6da3c5b58a083ce8648a8d70a
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-12-16 10:29:21 -0500

    Time: 61 ms (84.67%), Space: 13.8 MB (74.62%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -3,11 +3,8 @@ class Solution:
         i = 0
         output = 0
         while i<len(accounts):
-            j = 0
             total = 0
-            while j < len(accounts[i]):
-                total += accounts[i][j]
-                j += 1
+            total = sum(accounts[i])
             if total > output:
                 output = total
             i+= 1
