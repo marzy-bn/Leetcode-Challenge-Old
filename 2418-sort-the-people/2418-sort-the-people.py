4d93eea131a0f404b1c474d6ac10fe80e0502c63
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2022-12-22 11:27:25 -0500

    Time: 125 ms (87.23%), Space: 14.3 MB (88.44%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,17 +1,8 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        hmap = {}
-        for name,height in zip(names,heights):
-            hmap[height] = name
-        
+        tpls = sorted(zip(heights,names), reverse=True)
         out = []
-        for i in range(0,len(heights)):
-            h = 0
-            for height,name in hmap.items():
-                if h < height:
-                    h = height
-            out.append(hmap[h])
-            del hmap[h]
+        for a,b in tpls:
+            out.append(b)
         return out
-            
             
