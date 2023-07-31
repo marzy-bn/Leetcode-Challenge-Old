21a6211a219c93b9eea9dc4b9957eee014b4323a
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2023-07-31 12:44:11 -0400

    Time: 62 ms (74.73%), Space: 16.7 MB (99.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        #return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        hmap = {}
+        for val in s:
+            if val not in hmap:
+                hmap[val] = 1
+            else:
+                hmap[val] += 1
+                
+        for val in t:
+            if val not in hmap:
+                return False
+            else:
+                hmap[val] -= 1
+                if hmap[val] == 0:
+                    del hmap[val]
+        return True
