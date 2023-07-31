45f0c851d3292c3f5f0b8d10316b0861dfbf0e06
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2023-07-31 12:22:04 -0400

    Time: 54 ms (91.68%), Space: 17.5 MB (11.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,29 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        return sorted(s)==sorted(t) 
-    
-        """
-        if len(s) != len(t):
-            return False
-        
-        hmap = {}
-        
-        for letter in s:
-            if letter not in hmap:
-                hmap[letter] = 1
-            else:
-                hmap[letter] += 1
-        
-        #print(hmap)
-        
-        for letter in t:
-            if letter not in hmap:
-                return False
-            else:
-                hmap[letter] -= 1
-            if hmap[letter] == 0:
-                del hmap[letter]
-        
-        return True
-        """
+        return sorted(s) == sorted(t)
