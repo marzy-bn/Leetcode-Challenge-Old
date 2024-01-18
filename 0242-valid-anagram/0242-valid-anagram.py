207c3cd434afb54bb1ce52e1cd5b1b86c5912ea5
Marzieh Babaeianjelodar <102484566+marzy-bn@users.noreply.github.com>
2024-01-18 17:37:41 -0500

    Time: 33 ms (99.42%), Space: 17.7 MB (27.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        hmap1 = {}
+        hmap2 = {}
+        for item in s:
+            hmap1[item] = hmap1.get(item,0) + 1
+        for item in t:
+            hmap2[item] = hmap2.get(item,0) + 1
+        
+        return hmap1 == hmap2
+            
             
